a80188eca9bd816b9f558aaf0e07ac4463e5b58a
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-03-19 12:14:28 -0300

    Create User.java

---
+++ src/main/java/com/adriel/user_api/model/User.java
@@ -0,0 +1,39 @@
+package com.adriel.user_api.model;
+
+import java.util.Date;
+
+import com.adriel.user_api.dto.UserDTO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private Date dataCadastro;
+
+    public static User convert(UserDTO userDTO) {
+        User user = new User();
+        user.setNome(userDTO.getNome());
+        user.setEndereco(userDTO.getEndereco());
+        user.setCpf(userDTO.getCpf());
+        user.setEmail(userDTO.getEmail());
+        user.setTelefone(userDTO.getTelefone());
+        user.setDataCadastro(userDTO.getDataCadastro());
+        return user;
+    }
+}
