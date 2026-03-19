85b526a7a3d41b1fd7cedb7d3090502937d052c3
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-03-19 18:44:42 -0300

    feat: adds `V1__create_user_table.sql` migration

--- src/main/java/com/adriel/user_api/controller/UserController.java
+++ src/main/java/com/adriel/user_api/controller/UserController.java
@@ -1,19 +1,19 @@
 package com.adriel.user_api.controller;
 
 import com.adriel.user_api.dto.UserDTO;
+import com.adriel.user_api.service.UserService;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import jakarta.annotation.PostConstruct;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -21,76 +21,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 public class UserController {
 
-    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
-
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO(); 
-        userDTO.setNome("Eduardo"); 
-        userDTO.setCpf("123"); 
-        userDTO.setEndereco("Rua a"); 
-        userDTO.setEmail("eduardo@email.com"); 
-        userDTO.setTelefone("1234-3454"); 
-        userDTO.setDataCadastro(new Date());
-
-        UserDTO userDTO2 = new UserDTO(); 
-        userDTO2.setNome("Luiz"); 
-        userDTO2.setCpf("456"); 
-        userDTO2.setEndereco("Rua b"); 
-        userDTO2.setEmail("luiz@email.com"); 
-        userDTO2.setTelefone("1234-3454"); 
-        userDTO2.setDataCadastro(new Date());
-
-        UserDTO userDTO3 = new UserDTO(); 
-        userDTO3.setNome("Bruna");
-        userDTO3.setCpf("678"); 
-        userDTO3.setEndereco("Rua c"); 
-        userDTO3.setEmail("bruna@email.com"); 
-        userDTO3.setTelefone("1234-3454"); 
-        userDTO3.setDataCadastro(new Date()); 
-        
-        usuarios.add(userDTO); 
-        usuarios.add(userDTO2); 
-        usuarios.add(userDTO3);
-    }
+    @Autowired
+    private UserService userService;
     
     @GetMapping("/")
     public String getMessage() {
         return "Spring boot is Working!";
     }
 
-    @GetMapping("/users")
+    @GetMapping("/user/")
     public List<UserDTO> getUsers() {
+        List<UserDTO> usuarios = userService.getAll();
         return usuarios;
     }
 
-    @GetMapping("/users/{cpf}")
-    public UserDTO getUsersByCPF(@PathVariable String cpf) {
-
-        for (UserDTO userFilter: usuarios) {
-            if (userFilter.getCpf().equals(cpf)) {
-                return userFilter;
-            }
-        }
-
-        return null;
+    @GetMapping("/user/{id}")
+    UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
-    @PostMapping("/newUser")
-    UserDTO insert(@RequestBody UserDTO userDTO) {
-        userDTO.setDataCadastro(new Date());
-        usuarios.add(userDTO);
-        return userDTO;
+    @PostMapping("/user")
+    UserDTO newUser(@RequestBody UserDTO userDTO) {
+        return userService.save(userDTO);
     }
 
-    @DeleteMapping("/user/{cpf}")
-    public boolean remove(@PathVariable String cpf) {
-        for (UserDTO userFilter: usuarios) {
-            if (userFilter.getCpf().equals(cpf)) {
-                usuarios.remove(userFilter);
-                return true;
-            }
-        }
-        return false;
+    @GetMapping("/user/cpf/{cpf}")
+    public UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
     }
+
+    @DeleteMapping("/user/{id}")
+    public UserDTO delete(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping("/user/search")
+    public List<UserDTO> queryByName(@RequestParam(name = "nome", required = true)
+                                    String nome) {
+
+        return userService.queryByName(nome);
+    }
+    
 }
