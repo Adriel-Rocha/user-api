694ca80b0f6ec8f81780ce8064aab5a499999986
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-03-19 12:14:31 -0300

    Update UserDTO.java

--- src/main/java/com/adriel/user_api/dto/UserDTO.java
+++ src/main/java/com/adriel/user_api/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.adriel.user_api.dto;
 
 import java.util.Date;
 
+import com.adriel.user_api.model.User;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +16,15 @@ public class UserDTO {
     private String email;
     private String telefone;
     private Date dataCadastro;
+
+    public static UserDTO convert(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome(user.getNome());
+        userDTO.setEndereco(user.getEndereco());
+        userDTO.setCpf(user.getCpf());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setTelefone(user.getTelefone());
+        userDTO.setDataCadastro(user.getDataCadastro());
+        return userDTO;
+    }
 }
